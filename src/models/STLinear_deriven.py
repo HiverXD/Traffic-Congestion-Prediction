4e99413b59ca775e64b4da2edf7d12d0ab152964
HiverXD <158482745+HiverXD@users.noreply.github.com>
2025-05-30 21:13:38 +0900

    (add) STLinear with one head, six layer

---
+++ src/models/STLinear_deriven.py
@@ -0,0 +1,358 @@
+#STAEFORMER
+
+import torch.nn as nn
+import torch
+from torchinfo import summary
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
+
+class DLinearTemporal(nn.Module):
+    def __init__(self, in_steps, out_steps, num_nodes, kernel_size, individual=True):
+        super().__init__()
+        self.decomp = series_decomp(kernel_size)
+        self.in_steps, self.out_steps = in_steps, out_steps
+        self.num_nodes = num_nodes
+        self.individual = individual
+
+        if individual:
+            self.lin_season = nn.ModuleList([
+                nn.Linear(in_steps, out_steps) for _ in range(num_nodes)
+            ])
+            self.lin_trend = nn.ModuleList([
+                nn.Linear(in_steps, out_steps) for _ in range(num_nodes)
+            ])
+        else:
+            self.lin_season = nn.Linear(in_steps, out_steps)
+            self.lin_trend  = nn.Linear(in_steps, out_steps)
+
+    def forward(self, x, dim=1):
+        # x: [B, in_steps, num_nodes, model_dim]
+        B, T, N, D = x.shape
+        # first collapse model_dim into channel (we do DLinear on each feature separately)
+        x = x.permute(0, 2, 3, 1).reshape(B*N*D, T)  # [B·N·D, T]
+        res, mean = self.decomp(x.unsqueeze(-1))  # decomp expects shape [B·N·D, T, 1]
+        res = res.squeeze(-1); mean = mean.squeeze(-1)
+
+        # apply linear
+        if self.individual:
+            # but now each node & each feature has its own linear?
+            # simplest is to ignore feature-dim and just apply same for each D
+            out_res = torch.stack([
+                self.lin_season[n](res[B*n*D:(B*(n+1)*D), :])  for n in range(N)
+            ], dim=1)  # [B, N, D, out_steps]
+            out_mean = torch.stack([
+                self.lin_trend[n](mean[B*n*D:(B*(n+1)*D), :])   for n in range(N)
+            ], dim=1)
+        else:
+            out_res  = self.lin_season(res)  # [B·N·D, out_steps]
+            out_mean = self.lin_trend(mean)
+
+        out = out_res + out_mean
+        out = out.view(B, N, D, self.out_steps).permute(0, 3, 1, 2)
+        # → [B, out_steps, num_nodes, model_dim]
+        return out
+
+
+
+import numpy as np
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+#from torch_scatter import scatter
+from torch.nn.init import constant_, xavier_normal_, xavier_uniform_
+
+from torch_geometric.graphgym.models.layer import LayerConfig
+from torch_geometric.graphgym.config import cfg
+from torch_geometric.graphgym.register import register_layer
+
+
+
+
+
+class AttentionLayer(nn.Module):
+    """Perform attention across the -2 dim (the -1 dim is `model_dim`).
+
+    Make sure the tensor is permuted to correct shape before attention.
+
+    E.g.
+    - Input shape (batch_size, in_steps, num_nodes, model_dim).
+    - Then the attention will be performed across the nodes.
+
+    Also, it supports different src and tgt length.
+
+    But must `src length == K length == V length`.
+
+    """
+
+    def __init__(self, model_dim, num_heads=8, mask=False):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+        self.mask = mask
+
+        self.head_dim = model_dim // num_heads
+
+        self.FC_Q = nn.Linear(model_dim, model_dim)
+        self.FC_K = nn.Linear(model_dim, model_dim)
+        self.FC_V = nn.Linear(model_dim, model_dim)
+
+        self.out_proj = nn.Linear(model_dim, model_dim)
+
+    def forward(self, query, key, value):
+        # Q    (batch_size, ..., tgt_length, model_dim)
+        # K, V (batch_size, ..., src_length, model_dim)
+        batch_size = query.shape[0]
+        tgt_length = query.shape[-2]
+        src_length = key.shape[-2]
+
+        query = self.FC_Q(query)
+        key = self.FC_K(key)
+        value = self.FC_V(value)
+
+        # Qhead, Khead, Vhead (num_heads * batch_size, ..., length, head_dim)
+        query = torch.cat(torch.split(query, self.head_dim, dim=-1), dim=0)
+        key = torch.cat(torch.split(key, self.head_dim, dim=-1), dim=0)
+        value = torch.cat(torch.split(value, self.head_dim, dim=-1), dim=0)
+
+        key = key.transpose(
+            -1, -2
+        )  # (num_heads * batch_size, ..., head_dim, src_length)
+
+        attn_score = (
+            query @ key
+        ) / self.head_dim**0.5  # (num_heads * batch_size, ..., tgt_length, src_length)
+
+        if self.mask:
+            mask = torch.ones(
+                tgt_length, src_length, dtype=torch.bool, device=query.device
+            ).tril()  # lower triangular part of the matrix
+            attn_score.masked_fill_(~mask, -torch.inf)  # fill in-place
+
+        attn_score = torch.softmax(attn_score, dim=-1)
+        out = attn_score @ value  # (num_heads * batch_size, ..., tgt_length, head_dim)
+        out = torch.cat(
+            torch.split(out, batch_size, dim=0), dim=-1
+        )  # (batch_size, ..., tgt_length, head_dim * num_heads = model_dim)
+
+        out = self.out_proj(out)
+
+        return out
+
+
+class SelfAttentionLayer(nn.Module):
+    def __init__(
+        self, model_dim, feed_forward_dim=2048, num_heads=8, dropout=0, mask=False
+    ):
+        super().__init__()
+
+        self.attn = AttentionLayer(model_dim, num_heads, mask)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(model_dim, feed_forward_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(feed_forward_dim, model_dim),
+        )
+        self.ln1 = nn.LayerNorm(model_dim)
+        self.ln2 = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, dim=-2):
+        x = x.transpose(dim, -2)
+        # x: (batch_size, ..., length, model_dim)
+        residual = x
+        out = self.attn(x, x, x)  # (batch_size, ..., length, model_dim)
+        out = self.dropout1(out)
+        out = self.ln1(residual + out)
+
+        residual = out
+        out = self.feed_forward(out)  # (batch_size, ..., length, model_dim)
+        out = self.dropout2(out)
+        out = self.ln2(residual + out)
+
+        out = out.transpose(dim, -2)
+        return out
+
+
+class STLinear(nn.Module):
+    def __init__(
+        self,
+        num_nodes,
+        kernel_size,
+        in_steps=12,
+        out_steps=3,
+        steps_per_day=480,
+        input_dim=3,
+        output_dim=3,
+        input_embedding_dim=24,
+        tod_embedding_dim=24,
+        dow_embedding_dim=24,
+        spatial_embedding_dim=0,
+        adaptive_embedding_dim=80,
+        feed_forward_dim=256,
+        num_heads=4,
+        num_layers=3,
+        dropout=0.1,
+        use_mixed_proj=True,
+    ):
+        super().__init__()
+
+        self.num_nodes = num_nodes
+        self.in_steps = in_steps
+        self.out_steps = out_steps
+        self.steps_per_day = steps_per_day
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_embedding_dim = input_embedding_dim
+        self.tod_embedding_dim = tod_embedding_dim
+        self.dow_embedding_dim = dow_embedding_dim
+        self.spatial_embedding_dim = spatial_embedding_dim
+        self.adaptive_embedding_dim = adaptive_embedding_dim
+        self.model_dim = (
+            input_embedding_dim
+            + tod_embedding_dim
+            + dow_embedding_dim
+            + spatial_embedding_dim
+            + adaptive_embedding_dim
+        )
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+        self.use_mixed_proj = use_mixed_proj
+
+        self.input_proj = nn.Linear(input_dim, input_embedding_dim)
+        if tod_embedding_dim > 0:
+            self.tod_embedding = nn.Embedding(steps_per_day, tod_embedding_dim)
+        if dow_embedding_dim > 0:
+            self.dow_embedding = nn.Embedding(7, dow_embedding_dim)
+        if spatial_embedding_dim > 0:
+            self.node_emb = nn.Parameter(
+                torch.empty(self.num_nodes, self.spatial_embedding_dim)
+            )
+            nn.init.xavier_uniform_(self.node_emb)
+        if adaptive_embedding_dim > 0:
+            self.adaptive_embedding = nn.init.xavier_uniform_(
+                nn.Parameter(torch.empty(in_steps, num_nodes, adaptive_embedding_dim))
+            )
+
+        if use_mixed_proj:
+            self.output_proj = nn.Linear(
+                in_steps * self.model_dim, out_steps * output_dim
+            )
+        else:
+            self.temporal_proj = nn.Linear(in_steps, out_steps)
+            self.output_proj = nn.Linear(self.model_dim, self.output_dim)
+
+        self.lin_layers_t = nn.ModuleList([
+        DLinearTemporal(
+        in_steps=self.in_steps,
+        out_steps=self.in_steps,   # keep same length so stacking works
+        num_nodes=self.num_nodes,
+        individual=False,
+        kernel_size=kernel_size,
+    )
+    for _ in range(num_layers)
+])
+
+        self.attn_layers_s = nn.ModuleList(
+            [
+                SelfAttentionLayer(self.model_dim, feed_forward_dim, num_heads, dropout)
+                for _ in range(num_layers)
+            ]
+        )
+
+    def forward(self, x,edge_index=None,edge_attr=None):
+        # x: (batch_size, in_steps, num_nodes, input_dim+tod+dow=3)
+        batch_size = x.shape[0]
+
+        if self.tod_embedding_dim > 0:
+            tod = x[..., 3]/self.steps_per_day
+        if self.dow_embedding_dim > 0:
+            dow = x[..., 4]
+        x = x[..., : self.input_dim]
+
+        x = self.input_proj(x)  # (batch_size, in_steps, num_nodes, input_embedding_dim)
+        features = [x]
+        if self.tod_embedding_dim > 0:
+            tod_emb = self.tod_embedding(
+                (tod * self.steps_per_day).long()
+            )  # (batch_size, in_steps, num_nodes, tod_embedding_dim)
+            features.append(tod_emb)
+        if self.dow_embedding_dim > 0:
+            dow_emb = self.dow_embedding(
+                dow.long()
+            )  # (batch_size, in_steps, num_nodes, dow_embedding_dim)
+            features.append(dow_emb)
+        if self.spatial_embedding_dim > 0:
+            spatial_emb = self.node_emb.expand(
+                batch_size, self.in_steps, *self.node_emb.shape
+            )
+            features.append(spatial_emb)
+        if self.adaptive_embedding_dim > 0:
+            adp_emb = self.adaptive_embedding.expand(
+                size=(batch_size, *self.adaptive_embedding.shape)
+            )
+            features.append(adp_emb)
+        x = torch.cat(features, dim=-1)  # (batch_size, in_steps, num_nodes, model_dim)
+
+        for lin in self.lin_layers_t: 
+            x = lin(x, dim=1)
+        for attn in self.attn_layers_s:
+            x = attn(x, dim=2)
+        # (batch_size, in_steps, num_nodes, model_dim)
+
+        if self.use_mixed_proj:
+            out = x.transpose(1, 2)  # (batch_size, num_nodes, in_steps, model_dim)
+            out = out.reshape(
+                batch_size, self.num_nodes, self.in_steps * self.model_dim
+            )
+            out = self.output_proj(out).view(
+                batch_size, self.num_nodes, self.out_steps, self.output_dim
+            )
+            out = out.transpose(1, 2)  # (batch_size, out_steps, num_nodes, output_dim)
+        else:
+            out = x.transpose(1, 3)  # (batch_size, model_dim, num_nodes, in_steps)
+            out = self.temporal_proj(
+                out
+            )  # (batch_size, model_dim, num_nodes, out_steps)
+            out = self.output_proj(
+                out.transpose(1, 3)
+            )  # (batch_size, out_steps, num_nodes, output_dim)
+
+        return out
+
+
+if __name__ == "__main__":
+    model = custom_model(50, 12, 3)
+    summary(model, [64, 12, 207, 3])
